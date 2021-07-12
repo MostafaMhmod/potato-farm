09cca8d359bd998054158355694105a5196b14dc
Moustafa.mahm@gmail.com <moustafa.mahm@gmail.com>
2021-07-12 17:10:10 +0200

    Feat(deployment):updated the deployment script

--- scripts/deployment.py
+++ scripts/deployment.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
-from brownie import Lambo, Usd, accounts
+from brownie import accounts, Potato, Usd, SimpleChef
 
+account=accounts.load('testing')
+usd_token=None
+potato_token=None
+simple_chef=None
 
 def main():
-    usd_token.deploy("Usd", "USD", 18, 1e6, {'from': accounts[0]})
-    lambo_token.deploy("Lambo", "LMB", 18, 1e8, {'from': accounts[0]})
-
+    usd_token = Usd.deploy("Usd", "USD", 18, 1e24, {'from': account}, publish_source=True)
+    potato_token = Potato.deploy("Potato", "PTO", 18, 0, {'from': account}, publish_source=True)
+    simple_chef = SimpleChef.deploy(potato_token.address, usd_token.address,{'from': account}, publish_source=True)
+    potato_token.updateAdmin(simple_chef.address,{'from': account})
     return
+    
